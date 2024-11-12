747147debe86966d8e323b0dbf9cc1f0c3b1b289
ARINDAM BHANDARI <arindambhandari5@gmail.com>
2024-11-12 21:39:09 +0530

    looking password

--- public/Newpassword.js
+++ public/Newpassword.js
@@ -22,3 +22,18 @@ changePwBtn.addEventListener('click', () => {
             alert("Internal Server Error"); 
         })
     })
+
+    const eyeIcon = document.getElementById('eye-icon');
+    const passwordInput = document.getElementById('New-password');
+
+    eyeIcon.addEventListener('click', () => {
+        if (passwordInput.type === 'password') {
+            passwordInput.type = 'text';
+            eyeIcon.classList.remove('fa-eye');
+            eyeIcon.classList.add('fa-eye-slash');
+        } else {
+            passwordInput.type = 'password';
+            eyeIcon.classList.remove('fa-eye-slash');
+            eyeIcon.classList.add('fa-eye');
+        }
+    });
