a29a6604c2311884f0abf3946fe2f4f6f952b16c
ARINDAM BHANDARI <arindambhandari5@gmail.com>
2024-12-02 09:12:38 +0530

    Implement MVC

--- routes/user.route.js
+++ routes/user.route.js
@@ -27,52 +27,60 @@ const {
 
 
 // get register page
-userRouter.get("/registration", renderRegistrationPage);
-// registration
-userRouter.post("/registration", registration)
+userRouter.get("/registration", renderRegistrationPage);  //get:: /user/registration
+
+
+// User registration
+userRouter.post("/registration", registration) // Post:: /user/registration
 
 // verify email
-userRouter.get("/verify-email/:token", varifyEmailToken);
-// login page render
-userRouter.get("/login", renderLoginPage)
+userRouter.get("/verify-email/:token", varifyEmailToken);// get:: /user/verify-email
 
-// login
-userRouter.post("/login", login)
+
+// login page render
+userRouter.get("/login", renderLoginPage) // get:: /user/login
+
+// user login
+userRouter.post("/login", login) // post:: /user/login
 
 // render change password page
-userRouter.get("/Changepassword", rederChangePasswordPage)
+userRouter.get("/Changepassword", rederChangePasswordPage) // get:: /user/Changepassword
 
 // email page render
-userRouter.get("/resendEmail", renderEmailpage)
-// get new password page
-userRouter.get("/newPassword", renderNewPasswordPage)
-// resend verification email
-userRouter.post("/resend-verification-email", resendVerificationEmail);
-// send Password to email
-userRouter.post("/password-link", sendPasswordChangeLinkToEmail);
-// change password
+userRouter.get("/resendEmail", renderEmailpage) // get:: /user/resendEmail
 
-userRouter.post("/New-Password", setNewPassword)
+// get new password page
+userRouter.get("/newPassword", renderNewPasswordPage) // get:: /user/newPassword
+
+// resend verification email
+userRouter.post("/resend-verification-email", resendVerificationEmail); // post:: /user/resend-verification-email
+
+// send Password to email
+userRouter.post("/password-link", sendPasswordChangeLinkToEmail); // post:: /user/password-link
+
+// change password
+userRouter.post("/New-Password", setNewPassword) // post:: /user/New-Password
+
 // Dashboard
-userRouter.get("/dashboard", isUserAuth, renderDashboard)
+userRouter.get("/dashboard", isUserAuth, renderDashboard) // get:: /user/dashboard
 
 // logout
-userRouter.post("/logout", isUserAuth, logout)
+userRouter.post("/logout", isUserAuth, logout) // post:: /user/logout
 
 // logout all
-userRouter.post("/logoutAll", isUserAuth, logoutFromAlldevices);
+userRouter.post("/logoutAll", isUserAuth, logoutFromAlldevices); // post:: /user/logout
 
 // get user
-userRouter.get('/get-user', isUserAuth, getUser);
+userRouter.get('/get-user', isUserAuth, getUser); // get:: /user/get-user
 
 // upload DP
-userRouter.post('/uploadDP', isUserAuth, uploadDp);
+userRouter.post('/uploadDP', isUserAuth, uploadDp); // post:: /user/uploadDP
 
 // get DP
-userRouter.get('/get-userDP', isUserAuth, getDP);
+userRouter.get('/get-userDP', isUserAuth, getDP); // get:: /user/get-userDP
 
 // delete user
-userRouter.post('/delete-user', isUserAuth, deleteUser);
+userRouter.post('/delete-user', isUserAuth, deleteUser); // post:: /user/delete-user
 
 module.exports = userRouter;
 
